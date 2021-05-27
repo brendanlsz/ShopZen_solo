843014525589eebcc2de9779df73af52c7c5b32a
Nai Jie <69462657+naijie2108@users.noreply.github.com>
2021-05-27 21:46:51 +0800

    responsive form

--- src/components/AuthWrapper/index.js
+++ src/components/AuthWrapper/index.js
@@ -5,13 +5,11 @@ const AuthWrapper = ({ headline, children }) => {
   return (
     <div className="formwrap">
       <div className="form-body">
-        <div className="row">
-          <div className="form-holder">
-            <div className="form-content">
-              <div className="form-items">
-                {headline && <h3>{headline}</h3>}
-                <div className="children">{children && children}</div>
-              </div>
+        <div className="form-holder">
+          <div className="form-content">
+            <div className="form-items">
+              {headline && <h3>{headline}</h3>}
+              <div className="children">{children && children}</div>
             </div>
           </div>
         </div>
