a807790451febbdcff292fedd7bdc8329f565afb
Nai Jie <69462657+naijie2108@users.noreply.github.com>
2021-05-29 20:59:07 +0800

    Forms

--- src/components/AuthWrapper/index.js
+++ src/components/AuthWrapper/index.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import './styles.scss';
+import React from "react";
+import "./styles.scss";
 
 const AuthWrapper = ({ headline, children }) => {
   return (
-    <div className="authWrapper">
-      <div className="wrap">
-        {headline && <h2>{headline}</h2>}
-
-        <div className="children">
-          {children && children}
+    <div className="formwrap">
+      <div className="form-body">
+        <div className="form-holder">
+          <div className="form-content">
+            <div className="form-items">
+              {headline && <h3>{headline}</h3>}
+              <div className="children">{children && children}</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default AuthWrapper;
--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,98 +1,104 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link, useHistory } from 'react-router-dom';
-import { emailSignInStart, googleSignInStart } from './../../redux/User/user.actions';
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useHistory } from "react-router-dom";
+import {
+  emailSignInStart,
+  googleSignInStart,
+} from "./../../redux/User/user.actions";
 
-import './styles.scss';
+import "./styles.scss";
 
-import AuthWrapper from './../AuthWrapper';
-import FormInput from './../forms/FormInput';
-import Button from './../forms/Button';
+import AuthWrapper from "./../AuthWrapper";
+import FormInput from "./../forms/FormInput";
+import Buttons from "./../forms/Button";
 
 const mapState = ({ user }) => ({
-  currentUser: user.currentUser
+  currentUser: user.currentUser,
 });
 
-const SignIn = props => {
+const SignIn = (props) => {
   const dispatch = useDispatch();
   const history = useHistory();
   const { currentUser } = useSelector(mapState);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   useEffect(() => {
     if (currentUser) {
       resetForm();
-      history.push('/');
+      history.push("/");
     }
-
   }, [currentUser]);
 
   const resetForm = () => {
-    setEmail('');
-    setPassword('');
+    setEmail("");
+    setPassword("");
   };
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     dispatch(emailSignInStart({ email, password }));
-  }
+  };
 
   const handleGoogleSignIn = () => {
     dispatch(googleSignInStart());
-  }
+  };
 
   const configAuthWrapper = {
-    headline: 'LogIn'
+    headline: "Log In",
   };
 
   return (
     <AuthWrapper {...configAuthWrapper}>
-      <div className="formWrap">
+      <div>
+        <p>Key in your details below</p>
         <form onSubmit={handleSubmit}>
-
           <FormInput
             type="email"
             name="email"
             value={email}
             placeholder="Email"
-            handleChange={e => setEmail(e.target.value)}
+            handleChange={(e) => setEmail(e.target.value)}
           />
-
           <FormInput
             type="password"
             name="password"
             value={password}
             placeholder="Password"
-            handleChange={e => setPassword(e.target.value)}
+            handleChange={(e) => setPassword(e.target.value)}
           />
-
-          <Button type="submit">
-            LogIn
-          </Button>
-
-          <div className="socialSignin">
-            <div className="row">
-              <Button onClick={handleGoogleSignIn}>
-                Sign in with Google
-              </Button>
-            </div>
+          <p>
+            Forgot password? Reset it <Link to="/recovery">here</Link>
+          </p>
+          <div className="form-button mt-3 d-flex justify-content-center">
+            <Buttons id="submit" type="submit">
+              Login
+            </Buttons>
+          </div>
+          <p className="text-center my-4">Or</p>
+          <div className="d-flex justify-content-center">
+            <button
+              className="btn btn-lg btn-danger loginbtn"
+              onClick={handleGoogleSignIn}
+            >
+              Sign in with Google
+            </button>
           </div>
 
-          <div className="links">
-            <Link to="/registration">
-              Register
-            </Link>
-            {` | `}
-            <Link to="/recovery">
-              Reset Password
-            </Link>
+          <hr></hr>
+          <div className="d-flex justify-content-center">
+            <p className="d-flex align-items-center flex-direction: column">
+              Don't have an account?
+            </p>
           </div>
 
+          <Link to="/registration">
+            <Buttons className="btnblock">Register Here</Buttons>
+          </Link>
         </form>
       </div>
     </AuthWrapper>
   );
-}
+};
 
 export default SignIn;
